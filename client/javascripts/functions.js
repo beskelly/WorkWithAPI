1a1808661b606795ad41a7e2d7d9a348e0d94707
Blake Skelly <bskelly@uoregon.edu>
2020-04-24 11:30:48 -0700

    Add requirements 3 and 4

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -14,3 +14,49 @@ let containsDuplicates = s => {
   }
   return false;
 };
+
+
+function reverseWord(word) {
+    var newWord = "";
+    for (var i = word.length - 1; i >= 0; i--) {
+        newWord += word[i];
+    }
+    return newWord;
+}
+
+
+let revWords1 = (sentence) => {
+  let wordArray = sentence.split(), result = "";
+
+  for (var i = 0; i < wordArray.length; i++) {
+    let currentWord = wordArray[i];
+    let reversedWord = reverseWord(currentWord);
+    result += reversedWord;
+  }
+
+  return result;
+};
+
+
+let revWords2 = (sentence) => {
+  let wordArray = sentence.split(), result = "";
+
+  wordArray.forEach(function(item) {
+    let reversedWord = reverseWord(item);
+    result += reversedWord;
+  });
+
+  return result;
+};
+
+
+let revWords3 = (sentence) => {
+  let wordArray = sentence.split(), result = "";
+
+  for (var item of wordArray) {
+    let reversedWord = reverseWord(item);
+    result += reversedWord;
+  }
+
+  return result;
+};
